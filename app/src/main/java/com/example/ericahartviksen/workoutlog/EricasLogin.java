b78aba7bfd4f18a4f3f4c54f01cad4b277962956
RACHEL HARTVIKSEN <rhartviksen@gmail.com>
2015-04-08 19:31:13 -0400

    First commit from Rachel

--- app/src/main/java/com/example/ericahartviksen/workoutlog/EricasLogin.java
+++ app/src/main/java/com/example/ericahartviksen/workoutlog/EricasLogin.java
@@ -7,7 +7,7 @@ import android.view.MenuItem;
 
 
 public class EricasLogin extends ActionBarActivity {
-
+    // Secret message to Erica
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
